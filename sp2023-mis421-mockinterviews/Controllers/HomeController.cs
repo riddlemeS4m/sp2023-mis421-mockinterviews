d28c5ed11b4410bf619cd9fb7e350d16bccd2b4f
Sam Riddle <swriddle@crimson.ua.edu>
2023-03-24 11:19:03 -0500

    Add project files.

---
+++ sp2023-mis421-mockinterviews/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sp2023_mis421_mockinterviews.Models;
+using System.Diagnostics;
+
+namespace sp2023_mis421_mockinterviews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sp2023-mis421-mockinterviews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sp2023_mis421_mockinterviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
