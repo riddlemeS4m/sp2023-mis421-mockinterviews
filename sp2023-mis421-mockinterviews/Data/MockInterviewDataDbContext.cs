df723134844f729384b5fb9a88af03991160b112
Sam Riddle <swriddle@crimson.ua.edu>
2023-04-15 15:03:45 -0500

    Interview Signup -Sam

--- sp2023-mis421-mockinterviews/Data/MockInterviewDataDbContext.cs
+++ sp2023-mis421-mockinterviews/Data/MockInterviewDataDbContext.cs
@@ -12,7 +12,6 @@ namespace sp2023_mis421_mockinterviews.Data
 
         public DbSet<FAQs>? FAQs { get; set; }
         public DbSet<Location>? Location { get; set; }
-        //public DbSet<sp2023_mis421_mockinterviews.Models.MockInterviewDb.Interviewer>? Interviewer { get; set; }
         public DbSet<sp2023_mis421_mockinterviews.Models.MockInterviewDb.InterviewEvent>? InterviewEvent { get; set; }
         public DbSet<sp2023_mis421_mockinterviews.Models.MockInterviewDb.LocationInterviewer>? LocationInterviewer { get; set; }
         public DbSet<sp2023_mis421_mockinterviews.Models.MockInterviewDb.SignupInterviewer>? SignupInterviewer { get; set; }
---
+++ sp2023-mis421-mockinterviews/Models/ViewModels/SignupInterviewerTimeslotsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using sp2023_mis421_mockinterviews.Models.MockInterviewDb;
+
+namespace sp2023_mis421_mockinterviews.Models.ViewModels
+{
+    public class SignupInterviewerTimeslotsViewModel
+    {
+        public List<Timeslot> Timeslots { get; set; }
+        public int[] SelectedEventIds { get; set; }
+        public int[] SelectedType { get; set; }
+    }
+}
