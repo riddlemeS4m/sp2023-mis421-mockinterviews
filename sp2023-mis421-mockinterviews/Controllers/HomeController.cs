d1d48ebba69b2b102c768f6128e0e7a4d11db574
Sam Riddle <swriddle@crimson.ua.edu>
2023-04-15 17:00:30 -0500

    Working Volunteer Home -Sam

--- sp2023-mis421-mockinterviews/Controllers/HomeController.cs
+++ sp2023-mis421-mockinterviews/Controllers/HomeController.cs
@@ -1,21 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sp2023_mis421_mockinterviews.Data;
+using sp2023_mis421_mockinterviews.Models.MockInterviewDb;
 using sp2023_mis421_mockinterviews.Models.ViewModels;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace sp2023_mis421_mockinterviews.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly MockInterviewDataDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, MockInterviewDataDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            IndexViewModel model = new IndexViewModel();
+            model.VolunteerEventViewModels = new List<VolunteerEventViewModel>();
+
+            if(User.IsInRole(RolesConstants.AdminRole) || User.IsInRole(RolesConstants.StudentRole))
+            {
+                var volunteerEvents = await _context.VolunteerEvent
+                    .Include(v => v.Timeslot)
+                    .ThenInclude(y => y.EventDate)
+                    .Where(v => v.StudentId == userId)
+                    .ToListAsync();
+                if(volunteerEvents != null)
+                {
+                    foreach (VolunteerEvent volunteerEvent in volunteerEvents)
+                    {
+                        model.VolunteerEventViewModels.Add(new VolunteerEventViewModel()
+                        {
+                            VolunteerEvent = volunteerEvent
+                        });
+                    }
+                }
+                
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ sp2023-mis421-mockinterviews/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sp2023_mis421_mockinterviews.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<VolunteerEventViewModel> VolunteerEventViewModels { get; set; }
+        public List<SignupInterviewerTimeslotsViewModel> SignupInterviewerTimeslotsViewModels { get;  set; }
+    }
+}
