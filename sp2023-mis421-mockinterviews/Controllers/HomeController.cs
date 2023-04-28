34175dcdc20b42ecf3581a47210ba1ba0fd3160d
Logan Thompson <lmthompson6@crimson.ua.edu>
2023-04-28 16:52:06 -0500

    Email reminders

--- sp2023-mis421-mockinterviews/Controllers/HomeController.cs
+++ sp2023-mis421-mockinterviews/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SendGrid.Helpers.Mail;
+using SendGrid;
 using sp2023_mis421_mockinterviews.Data;
 using sp2023_mis421_mockinterviews.Models.MockInterviewDb;
 using sp2023_mis421_mockinterviews.Models.UserDb;
@@ -167,6 +169,32 @@ namespace sp2023_mis421_mockinterviews.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> EmailStudents()
+        {
+            var uniqueUsers = _context.InterviewEvent.GroupBy(s => s.StudentId).Select(g => new { StudentId = g.Key });
+            foreach (var user in uniqueUsers)
+            {
+                var userFull = await _userManager.FindByIdAsync(user.StudentId);
+                var interviews = await _context.InterviewEvent.Include(x => x.Timeslot).ThenInclude(x => x.EventDate).Where(x => x.StudentId == user.StudentId).ToListAsync();
+                var client = new SendGridClient("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");
+                var from = new EmailAddress("mismockinterviews@gmail.com", "UA MIS Program Support");
+                var subject = "Mock Interviews Reminder";
+                var to = new EmailAddress(userFull.Email);
+                var plainTextContent = "";
+                var htmlContent = " <head>\r\n    <title>MIS Mock Interviews Reminder</title>\r\n    <style>\r\n      /* Define styles for the header */\r\n      header {\r\n        background-color: crimson;\r\n        color: white;\r\n        text-align: center;\r\n        padding: 20px;\r\n      }\r\n      \r\n      /* Define styles for the subheading */\r\n      .subheading {\r\n        color: black;\r\n        font-weight: bold;\r\n        margin: 20px 0;\r\n      }\r\n      \r\n      /* Define styles for the closing */\r\n      .closing {\r\n        font-style: italic;\r\n        margin-top: 20px;\r\n        text-align: center;\r\n      }\r\n    </style>\r\n  </head>\r\n  <body>\r\n    <header>\r\n      <h1>Hey, " + userFull.FirstName + "! Mock Interviews are coming up!</h1>\r\n    </header>\r\n    <div class=\"content\">\r\n      <p class=\"subheading\">\r\n        You have signed up to be interviewed during the following times:<br>";
+                foreach (var interview in interviews)
+                {
+                    htmlContent += interview.Timeslot.Time;
+                    htmlContent += " on ";
+                    htmlContent += interview.Timeslot.EventDate.Date;
+                }
+                htmlContent += "<br>This email serves as your final reminder that you have signed-up for Mock Interviews. Please be sure to arrive <u>15 minutes early</u> to your first interview time!\r\n      </p>\r\n      <p>\r\n        If you have any questions or concerns, please don't hesitate to contact us.\r\n      </p>\r\n      <p class=\"closing\">\r\n        Thank you, Program Support\r\n      </p>\r\n    </div>\r\n  </body>";
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = client.SendEmailAsync(msg);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
