deed3db4e737fbef21736660df884e5b1896c6f3
Sam Riddle <swriddle@crimson.ua.edu>
2024-09-08 00:49:51 -0500

    Add Areas -Sam

--- sp2023-mis421-mockinterviews/Program.cs
+++ sp2023-mis421-mockinterviews/Program.cs
@@ -245,6 +245,14 @@ namespace sp2023_mis421_mockinterviews
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
             app.MapRazorPages();
 
             //look at this later
